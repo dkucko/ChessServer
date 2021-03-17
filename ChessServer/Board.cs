8d0a1a80a7ed2ec9dc63dab2426e05b44a6a3928
daniel.kucko <kucko.daniel@freenet.de>
2021-03-17 09:55:37 +0100

    Projektdateien hinzufügen.

---
+++ ChessServer/Board.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessServer
+{
+    class Board
+    {
+        private char[] squares = new char[64];
+        private string playerToMove;
+        private string castlingRights;
+        private string enPassantMove;
+        private int numHalfMoves;
+        private int numTurns;
+        private Stack<string> fenHistory = new Stack<string>();
+
+        public Board(string fen="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+        {
+            this.ParseFen(fen);
+            this.fenHistory.Push(fen);
+        }
+
+        public void DoMove(string move)
+        {
+            // First do the move
+
+            // Then save the resulting fen to the history
+            this.fenHistory.Push(this.GenerateFen());
+        }
+
+        public void UndoMove()
+        {
+            // Pop the last fen from the history and parse it, thereby restoring the state before the last move
+            this.ParseFen(this.fenHistory.Pop());
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("  a b c d e f g h  ");
+            Console.WriteLine(" ----------------- ");
+            for (int row = 0; row < 8; row++)
+            {
+                Console.WriteLine(8 - row + "|" + String.Join(' ', this.squares.Skip(row * 8).Take(8).ToArray()) + "|" + (8 - row));
+            }
+            Console.WriteLine(" ----------------- ");
+            Console.WriteLine("  a b c d e f g h  ");
+            Console.WriteLine("FEN: " + this.GenerateFen());
+        }
+
+        private void ParseFen(string fen)
+        {
+            string[] groups = fen.Split(' ');
+
+            // First Group: Board Position
+            int currentRow = 0;
+            foreach (string row in groups[0].Split('/'))
+            {
+                for (int i = 0; i < row.Length; i++)
+                {
+                    if ('1' <= row[i] && row[i] <= '8')
+                    {
+                        for (int j = 0; j < Convert.ToInt32(row[i].ToString()); j++)
+                        {
+                            this.squares[currentRow * 8 + i + j] = ' ';
+                        }
+                    } else
+                    {
+                        this.squares[currentRow * 8 + i] = row[i];
+                    }
+                }
+                currentRow += 1;
+            }
+
+            // Second Group: Player to move
+            this.playerToMove = groups[1];
+
+            // Third Group: Castling rights
+            this.castlingRights = groups[2];
+
+            // Fourth Group: En Passant move
+            this.enPassantMove = groups[3];
+
+            // Fifth Group: Number of half moves
+            this.numHalfMoves = Convert.ToInt32(groups[4]);
+
+            // Sixth Group: Number of the current turn
+            this.numTurns = Convert.ToInt32(groups[5]);
+        }
+
+        private string GenerateFen()
+        {
+            string fen = "";
+            int skippedFields = 0;
+            string row = "";
+            for (int i = 1; i <= 64; i++)
+            {
+                if (this.squares[i - 1] == ' ') {
+                    skippedFields += 1;
+                } else
+                {
+                    if (skippedFields > 0)
+                    {
+                        row += skippedFields;
+                        skippedFields = 0;
+                    }
+                    row += this.squares[i - 1];
+                }
+
+                if (i % 8 == 0)
+                {
+                    if (skippedFields > 0)
+                    {
+                        row += skippedFields;
+                        skippedFields = 0;
+                    }
+                    fen += row;
+                    if (i != 64)
+                    {
+                        fen += '/';
+                    }
+                    row = "";
+                }
+            }
+
+            return fen + " " + this.playerToMove + " " + this.castlingRights + " " + this.enPassantMove + " " + this.numHalfMoves + " " + this.numTurns;
+        }
+    }
+}
---
+++ ChessServer/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace ChessServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 32168;
+            string fen;
+            // Parse command line options
+            for (int i = 0; i < args.Length; i+=2)
+            {
+                switch (args[i])
+                {
+                    case "-h":
+                    case "--help":
+                        PrintHelp();
+                        return;
+                    case "-p":
+                    case "--port":
+                        try
+                        {
+                            port = Convert.ToInt32(args[i + 1]);
+                            if (port < 1 || port > 65535)
+                            {
+                                Console.WriteLine("ERROR: Specified port is not within the valid port range (1-65535)");
+                                return;
+                            }
+                        } catch (IndexOutOfRangeException)
+                        {
+                            Console.WriteLine("ERROR: No port was specified");
+                            return;
+                        } catch
+                        {
+                            Console.WriteLine("ERROR: Port must be an integer");
+                            return;
+                        }
+                        break;
+                    case "-f":
+                    case "--fen":
+                        try
+                        {
+                            fen = args[i + 1];
+                        } catch (IndexOutOfRangeException)
+                        {
+                            Console.WriteLine("ERROR: No fen was specified");
+                            return;
+                        }
+                        break;
+                    case "-t":
+                    case "--time":
+                        try
+                        {
+                            string time = args[i + 1];
+                        } catch (IndexOutOfRangeException)
+                        {
+                            Console.WriteLine("ERROR: No time was specified");
+                            return;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine($"ERROR: Unkown command option: {args[i]}");
+                        return;
+                }
+            }
+
+
+
+            Board b = new Board();
+            b.Print();
+            Console.ReadLine();
+        }
+
+        static void PrintHelp()
+        {
+            Console.WriteLine(@"
+This is a simple Chess Server for two clients communicating via Sockets.
+
+Available Command Line Options:
+-h, --help: Print this help text and exit the program
+-p, --port: The Port on which the server should listen, default: 32168
+-f, --fen : The initial board position encoded in FEN Notation (MUST be surronded by double Quotes)
+-t, --time: The Time Control to use (all numbers will be interpreted as seconds)
+    examples:
+    -t 3  : White and black have a total of 3 seconds for play with no additional time per turn
+    -t 10+2 :  White and black have a total of 10 secondes for play with an addional two seconds per turn
+    -t 360:10+2 : White will have a total of 6 minutes with no bonus time, black will have a total of 10 seconds with two second bonus per turn
+            ");
+        }
+    }
+}
